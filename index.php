8713c831aa8f62c18cf3fdc12934268c63a9d799
eHypergrowth <86742270+eHypergrowth@users.noreply.github.com>
2022-09-12 19:22:37 -0600

    index.php
    reacomodo de sangria en documentacion

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 include('kernel/ldKernelSystem.php');
 session_start();
-if ($_SERVER['HTTPS'] == 'on') {										//LDDOC: Validamos que este entrando por HTTPS
-	$Path = dirname( __FILE__ );										//LDDOC: OBTENEMOS DIRECTORIO RAÍZ
+if ($_SERVER['HTTPS'] == 'on') {							//LDDOC: Validamos que este entrando por HTTPS
+	$Path = dirname( __FILE__ );							//LDDOC: OBTENEMOS DIRECTORIO RAÍZ
 	$App = ''. $Path .'/mvc/controllers/ldevelopSystem.php';			//LDDOC: Dirección del archivo matriz de la APP
-	if (file_exists($App)) {											//Verificamos que la librería matriz exista
+	if (file_exists($App)) {							//Verificamos que la librería matriz exista
 	        # code...
 			$module = new lindebKernel();
 			$module->ldInclude('/../libs/inputFilter/class.inputfilter.php');
 			$route = $module->route($_GET);
-	        include($App);												//LDDOC: Si la librería existe incluirla 
-			$start = New startSystem($route[0]);						//LDDOC: Iniciar la clase matriz del sistema con el constructor automático
-	}else{																//LDDOC: Si la librería no existe mostramos mensaje de inexistencia de librería
+	        include($App);								//LDDOC: Si la librería existe incluirla 
+			$start = New startSystem($route[0]);				//LDDOC: Iniciar la clase matriz del sistema con el constructor automático
+	}else{										//LDDOC: Si la librería no existe mostramos mensaje de inexistencia de librería
 		//echo 'La libreria no existe ' . $App;
-		echo 'The Library not found! Error: 404 Lybrary Master ', $App;	//LDDOC: Mostremos un mensaje de error en la carga de la librería
+		echo 'The Library not found! Error: 404 Lybrary Master ', $App;		//LDDOC: Mostremos un mensaje de error en la carga de la librería
 	}
 }else{
 	$hostDomain = $_SERVER['HTTP_HOST'];
