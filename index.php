ab6fe7702d077aeb198c7323ebcab4e146f905c1
eHypergrowth <86742270+eHypergrowth@users.noreply.github.com>
2024-07-06 12:22:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php
-//LDDOC-AUTHOR: ING. EMMANUEL RAMIREZ 
-//LDDOC-ALIAS: EHYPERGROWTH
-//LDDOC-EMAIL: emmanuelramirez@lindevelop.com, developer@ehypergrowth.com
-//LDDOC-WEBSITE: https://lindevelop.com/products/linPhpKernel
-//LDDOC-DATESTART: FECHA DE CREACION: 23/06/2018
-//LDDOC-DATEREPOSITORI: FECHA EN QUE SE CREO UN REPOSITORIO OFICIAL: 12/09/2022
-
+// LDDOC-AUTHOR: ING. EMMANUEL RAMIREZ 
+// LDDOC-ALIAS: EHYPERGROWTH
+// LDDOC-EMAIL: emmanuelramirez@lindevelop.com, developer@ehypergrowth.com
+// LDDOC-WEBSITE: https://lindevelop.com/products/linPhpKernel
+// LDDOC-DATESTART: FECHA DE CREACION: 23/06/2018
+// LDDOC-DATEREPOSITORI: FECHA EN QUE SE CREO UN REPOSITORIO OFICIAL: 12/09/2022
 
 include('kernel/ldKernelSystem.php');
 session_start();
-if ($_SERVER['HTTPS'] == 'on') {							//LDDOC: Validamos que este entrando por HTTPS
-	$Path = dirname( __FILE__ );							//LDDOC: OBTENEMOS DIRECTORIO RAÍZ
-	$App = ''. $Path .'/mvc/controllers/ldevelopSystem.php';			//LDDOC: Dirección del archivo matriz de la APP
-	if (file_exists($App)) {							//Verificamos que la librería matriz exista
-	        # code...
-			$module = new lindebKernel();
-			$module->ldInclude('/../libs/inputFilter/class.inputfilter.php');
-			$route = $module->route($_GET);
-	        include($App);								//LDDOC: Si la librería existe incluirla 
-			$start = New startSystem($route[0]);				//LDDOC: Iniciar la clase matriz del sistema con el constructor automático
-	}else{										//LDDOC: Si la librería no existe mostramos mensaje de inexistencia de librería
-		//echo 'La libreria no existe ' . $App;
-		echo 'The Library not found! Error: 404 Lybrary Master ', $App;		//LDDOC: Mostremos un mensaje de error en la carga de la librería
-	}
-}else{
-	$hostDomain = $_SERVER['HTTP_HOST'];
-	header("Location: https://$hostDomain/$module->route[0]");			//LDDOC: De no entrar por HTTPS forzamos la entrada redireccionando con HTTPS
-	//echo 'error no ssl';
+
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+    $path = dirname(__FILE__);
+    $appPath = $path . '/mvc/controllers/ldevelopSystem.php';
+
+    if (file_exists($appPath)) {
+        $module = new lindebKernel();
+        $module->ldInclude('/../libs/inputFilter/class.inputfilter.php');
+        $route = $module->route($_GET);
+
+        include($appPath);
+
+        $start = new startSystem($route[0]);
+    } else {
+        echo 'The Library not found! Error: 404 Library Master ' . htmlspecialchars($appPath, ENT_QUOTES, 'UTF-8');
+    }
+} else {
+    $hostDomain = $_SERVER['HTTP_HOST'];
+    header("Location: https://$hostDomain/" . htmlspecialchars($module->route[0], ENT_QUOTES, 'UTF-8'));
+    exit(); // Asegurarse de que el script se detiene después de la redirección.
 }
 ?>
